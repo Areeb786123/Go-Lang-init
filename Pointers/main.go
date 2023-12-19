4c22644264f24d4f1b47b7442cd3fdb385bfe3d1
areeb786 <areebrockzz786@gmail.com>
2023-12-20 00:39:12 +0530

    pointer

--- Pointers/main.go
+++ Pointers/main.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello")
+
+	number := 23
+	var ptr = &number
+	fmt.Println(ptr)  // this will print  the pointer address where is it stored output 0x1400000e0b0
+	fmt.Println(*ptr) // using * will tell the value of  the pointer output 23
+
 }
